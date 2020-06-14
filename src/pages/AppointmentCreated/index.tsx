1e4a997cb14da938a772e9d210bafdbec91427d2
Rafael Arantes <birutaibm@gmail.com>
2020-06-14 18:42:43 -0300

    Add page structure

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-import { useAuth } from '../../hooks/auth';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import { Container, Title, Description, OkButton, OkButtonText } from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth();
+  const { reset } = useNavigation();
+
+  const handleOk = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
 
   return (
-    <View style={{flex: 1, justifyContent: "center"}}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Segunda, dia 15 de junho de 2020 às 11:00h</Description>
+
+      <OkButton onPress={handleOk}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
